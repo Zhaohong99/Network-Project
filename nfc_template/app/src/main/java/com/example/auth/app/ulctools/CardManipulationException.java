c5c4840dc3c989724e4cef66a80562c140ad0e62
Liu Zhaohong <liuzh@LiudeMacBook-Pro.local>
2019-12-12 11:18:41 +0200

    template

---
+++ nfc_template/app/src/main/java/com/example/auth/app/ulctools/CardManipulationException.java
@@ -0,0 +1,8 @@
+package com.example.auth.app.ulctools;
+
+public class CardManipulationException extends Exception{
+    public CardManipulationException(String message) {
+        super(message);
+    }
+}
+
